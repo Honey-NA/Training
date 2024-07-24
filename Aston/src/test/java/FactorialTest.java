f889099aa300c8842c4ebf6a026b567e05522c78
Honey-NA <nikitamain@list.ru>
2024-07-24 21:31:30 +0300

    +

--- Aston/src/test/java/FactorialTest.java
+++ Aston/src/test/java/FactorialTest.java
@@ -2,11 +2,15 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class FactorialTest {
-
+    Factorial factorial = new Factorial();
+    @Test
+    public void testFactorialZero() {
+        int result = Factorial.factorial(0);
+        Assert.assertEquals(result,1);
+    }
     @Test
     public void testFactorial() {
-        Factorial factorial = new Factorial();
-        Assert.assertEquals(Factorial.factorial(0),1);
-        Assert.assertEquals(Factorial.factorial(5),120);
+        int result = Factorial.factorial(5);
+        Assert.assertEquals(result,120);
     }
 }
